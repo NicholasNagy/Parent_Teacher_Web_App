d70657cd1c3f38595524aa323e2e973389cc8800
KhaledBadran <43306217+KhaledBadran@users.noreply.github.com>
2018-10-31 12:04:31 -0400

    adds functionality issue #69
    node handles uploaded files

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,6 @@ var app = express();
 var bodyParser = require("body-parser");
 var mysql = require('mysql');
 var DBconnect = require ('./dbConfig');
-var login = require('./functions');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,34 +17,52 @@ router.post('/', function(req,res){
     var email=req.body.email_logIn;
     var pass = req.body.password_logIn;
 
-    let authenticationsuccess = new Promise(function(resolve, reject){
-      resolve(login.authenticate(email,pass));
+    //make querying statements, first one searches through the Parents table for
+    //the same email and password, while the other searches through the teachers table
+    var sql = "SELECT Email, Pass, Fname, Lname, isTeacher, ID from Users where Email='"+email+"' and Pass='"+pass+"';";
+
+
+    //below checks the query for the same email and password
+    pool.connection.query(sql, function (err, result){
+        if (err) throw err;
+        //underneath is the boolean for it
+
+        if(result.length>0){
+
+            if(result[0].Email==email&&result[0].Pass==pass){
+
+                console.log("Login Successful");
+                var posts = "SELECT Content, postID, Image FROM post where WallID='"+result[0].ID+"';";
+                pool.connection.query(posts, function (error, results) {
+
+                    if (error)
+                        throw error;
+
+                    var thename=result[0].Fname;
+                    // this variable is hidden in the homepage and will be used later to
+                    // collect data when routing to other pages
+                    var theID=result[0].ID;
+                    console.log(theID);
+
+
+                    if(result[0].isTeacher==0) {
+                        res.render('homepage', {posts: results, name: thename, userID: theID});
+                        console.log("parenthomepage");
+                    }
+                    else{
+                        res.render('parenthomepage', {posts: results, name:thename, WallID:theID, PosterID:theID});
+                        console.log("teacherhomepage");
+                    }
+                });
+            }
+        }
+
+        else{
+            console.log("unsuccessful login");
+            res.send("unsuccessful login");
+        };
+
     });
-
-    authenticationsuccess.then(function(user){
-      if (user.length==0){
-        console.log("unsuccessful login");
-        res.send("unsuccessful login");
-      }
-      else{
-        console.log("Login Successful");
-        let getPosts = new Promise(function(resolve, reject){
-          resolve(login.getWallPosts(user[0].ID));
-        });
-
-        getPosts.then(function(posts){
-          if(user[0].isTeacher==0){
-            res.render('homepage', {posts: posts, name: user[0].Fname, userID: user[0].ID});
-            console.log("parenthomepage");
-          }
-          else{
-            res.render('parenthomepage', {posts: posts, name:user[0].Fname, WallID:user[0].ID, userID:user[0].ID});
-            console.log("teacherhomepage");
-          }
-        });
-      }
-    })
-
 
 
 });
