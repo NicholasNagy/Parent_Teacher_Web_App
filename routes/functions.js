8ff6be4f28bfc0c673fa56f41dbc9cef0c0abf90
Khaled Badrna <43306217+KhaledBadran@users.noreply.github.com>
2018-11-14 13:59:27 -0500

    added create groups function

--- routes/functions.js
+++ routes/functions.js
@@ -65,16 +65,26 @@ var getWall = function(WallID, userID){
 
         var posts = "SELECT Fname, Content, postID, posterID, WallID, likes, Image FROM post join Users ON post.WallID='"+WallID+"' AND post.posterID=Users.ID ORDER BY post.postID DESC;";
 
+
         pool.connection.query(posts, function (error, results) {
           if (error)
               throw error;
 
+         var notification = [];
+
           let user = new Promise(function(resolve, reject){
+            for (let i=0; i<results.length; i++){
+              if (results[i].WallID != results[i].posterID) {
+                  notification.push(results[i]);
+                  console.log("testing notification: "+results[i].Fname);
+              }
+            }
+            console.log("Testing the notification string: "+notification[0]);
             resolve(getUser(userID));
           });
 
           user.then(function(theuser){
-            resolve({posts:results, name:theuser.Fname, WallID:WallID, userID:userID});
+            resolve({posts:results, name:theuser.Fname, WallID:WallID, userID:userID, notification: notification});
           });
 
         });
@@ -133,32 +143,36 @@ var viewProfile = function(userID){
   });
 }
 
+
+
+
 var creatingGroups = function (groupName, userID){
 
-  console.log("Creating A group");
-  console.log(groupName);
-
-  //CREATING SQL METHOD
-  var userName = "SELECT Fname,Lname FROM users where ID='"+userID+"';";
-
-  //INSERTING THE NEW POST
-  return new Promise(function(resolve, reject){
-
-    pool.connection.query(userName, function (error, result) {
-      if (error) throw error;
-
-      //Creating the group
-      var createGroup = "INSERT into groups (memberID,Fname,Lname, groupName) VALUES ('"+userID+"','"+result[0].Fname+"','"+result[0].Lname+"','"+groupName+"')";
-
-      pool.connection.query(createGroup, function (error, result0) {
-          if (error) throw error;
-
-          resolve("Group is added to DB");
+    console.log("Creating A group");
+    console.log(groupName);
+  
+    //CREATING SQL METHOD
+    var userName = "SELECT Fname,Lname FROM users where ID='"+userID+"';";
+  
+    //INSERTING THE NEW POST
+    return new Promise(function(resolve, reject){
+  
+      pool.connection.query(userName, function (error, result) {
+        if (error) throw error;
+  
+        //Creating the group
+        var createGroup = "INSERT into groups (memberID,Fname,Lname, groupName) VALUES ('"+userID+"','"+result[0].Fname+"','"+result[0].Lname+"','"+groupName+"')";
+  
+        pool.connection.query(createGroup, function (error, result0) {
+            if (error) throw error;
+  
+            resolve("Group is added to DB");
+        });
       });
     });
-  });
-
-};
+  
+  };
+  
 
 
 
